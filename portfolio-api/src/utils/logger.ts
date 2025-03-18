ebcde96fce630b2214cf012a3c4523a77c1b2ae6
Cécile Dubois <duboiscecilepro@gmail.com>
2025-03-18 12:32:41 +0100

    Nouvel essai de logger, fonctionne en local mais toujours aucun fichier/dossier sur PlanetHoster

--- portfolio-api/src/utils/logger.ts
+++ portfolio-api/src/utils/logger.ts
@@ -1,10 +1,49 @@
 /* eslint-disable no-console */
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const dirname = process.cwd();
-const logFilePath = path.join(dirname, 'logs', 'logs.log');
+// Vérifier le type de module
+// const isESM = typeof import.meta !== 'undefined';
+// console.log(process.cwd());
+// let logFilePath;
+// if (isESM) {
+// const currentFilePath = fileURLToPath(import.meta.url);
+// const currentDir = path.dirname(currentFilePath);
+//     logFilePath = path.join(currentDir, '../logs.log');
+// } else {
+//     const currentFilePath = __filename;
+//     const currentDir = path.dirname(currentFilePath);
+//     logFilePath = path.join(currentDir, '../logs.log');
+// }
 
+// console.log('logFilePath:', logFilePath);
+// const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
+// console.log('logStream:', logStream.path);
+
+// Créer une fonction pour obtenir le dirname compatible avec ESM et CommonJS
+const getDirname = (): string => {
+    if (typeof __dirname !== 'undefined') {
+        const currentFilePath = __filename; // CommonJS
+        return fileURLToPath(currentFilePath);
+        // return path.dirname(currentFilePath);
+        // return __dirname; // CommonJS
+    } else {
+        // ESM
+        const currentFilePath = path.dirname(fileURLToPath(import.meta.url));
+        const logDir = path.join(currentFilePath, '../', 'logs');
+        if (!fs.existsSync(logDir)) {
+            fs.mkdirSync(logDir, { recursive: true });
+        }
+
+        return logDir;
+    }
+};
+
+const dirname2 = getDirname();
+console.log('dirname:', dirname2);
+const logFilePath = path.join(dirname2, 'logs.log');
+console.log('logFilePath:', logFilePath);
 const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
 console.log('logStream:', logStream.path);
 
