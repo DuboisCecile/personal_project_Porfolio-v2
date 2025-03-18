ad84a5dbe88a60ceef487151f5ac913ee74af223
Cécile Dubois <duboiscecilepro@gmail.com>
2025-03-18 12:12:28 +0100

    Nouvel essai de logger, fonctionne en local mais toujours aucun fichier/dossier sur PlanetHoster

--- portfolio-api/src/utils/logger.ts
+++ portfolio-api/src/utils/logger.ts
@@ -2,50 +2,9 @@
 import fs from 'fs';
 import path from 'path';
 
-// Vérifier le type de module
-// const isESM = typeof import.meta !== 'undefined';
-// console.log(process.cwd());
-// let logFilePath;
-// if (isESM) {
-//     const currentFilePath = fileURLToPath(import.meta.url);
-//     const currentDir = path.dirname(currentFilePath);
-//     logFilePath = path.join(currentDir, '../logs.log');
-// } else {
-//     const currentFilePath = __filename;
-//     const currentDir = path.dirname(currentFilePath);
-//     logFilePath = path.join(currentDir, '../logs.log');
-// }
+const dirname = process.cwd();
+const logFilePath = path.join(dirname, 'logs', 'logs.log');
 
-// console.log('logFilePath:', logFilePath);
-// const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
-// console.log('logStream:', logStream.path);
-
-// Créer une fonction pour obtenir le dirname compatible avec ESM et CommonJS
-const getDirname = (): string => {
-    // if (typeof __dirname !== 'undefined') {
-    //     const currentFilePath = __filename; // CommonJS
-    //     return fileURLToPath(currentFilePath);
-    //     // return path.dirname(currentFilePath);
-    //     // return __dirname; // CommonJS
-    // } else {
-    //     // ESM
-    const filename = process.cwd();
-    return path.dirname(filename);
-    // }
-};
-console.log(process.cwd());
-
-// currentFilePath D:\INFORMATIQUE\PROJETS PERSOS\Portfolio-v2\portfolio-api\dist\dev\utils\logger.js
-// currentDir D:\INFORMATIQUE\PROJETS PERSOS\Portfolio-v2\portfolio-api\dist\dev\utils
-// logFilePath D:\INFORMATIQUE\PROJETS PERSOS\Portfolio-v2\portfolio-api\dist\dev\logs.log
-// logsDir D:\INFORMATIQUE\PROJETS PERSOS\Portfolio-v2\portfolio-api\dist\dev
-// logFilePath: D:\INFORMATIQUE\PROJETS PERSOS\Portfolio-v2\portfolio-api\dist\dev\logs.log
-// logStream: D:\INFORMATIQUE\PROJETS PERSOS\Portfolio-v2\portfolio-api\dist\dev\logs.log
-
-const dirname2 = getDirname();
-console.log('dirname:', dirname2);
-const logFilePath = path.join(dirname2, 'logs.log');
-console.log('logFilePath:', logFilePath);
 const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
 console.log('logStream:', logStream.path);
 
